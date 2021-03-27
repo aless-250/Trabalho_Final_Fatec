87bfd29c9ccd88fb7dae69f812a5bd1e03c1e536
aless-250 <alessandro.silva47@fatec.sp.gov.br>
2021-03-27 12:22:19 -0300

    Inclusão do projeto

---
+++ Projeto - SistemaGuaxinim/src/visao/CadUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package visao;
+
+/**
+ * Classe cirada para...
+ * @author Alessandro S Silva
+ * @Since Classe criada em &(date) as &(time)
+ */
+class CadUsuario {
+
+}//fim da classe
---
+++ Projeto - SistemaGuaxinim/src/visao/TelaBemVindos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package visao;
+
+/**--------------------------------------------------------------------------
+ * Sistema Guaxinim                                                         |
+ * Descrição: Trabalho TG                                                   |
+ * Sistema de inventario feito como conclusão do curso de ADS               |
+ * na Fatec de Santana de Parnaiba.                                         |
+ * @author Alessandro de Sousa da Silva                                     |
+ * Data: 08/03/2020                                                         |
+/----------------------------------------------------------------------------
+ */
+
+public class TelaBemVindos extends jInternalFrameTelaBemVindos {
+
+    static void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public TelaBemVindos() {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}//fim da classe
